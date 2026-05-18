ae72faf09de84e9afe68c52ac796a7eaaf716942
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2026-05-18 14:49:03 -0400

    add initial parts of website

--- materials/RenderToPDFandSlides.sh
+++
@@ -1,7 +0,0 @@
-for x in `ls *.qmd`
-   do
-      echo File: $x
-      quarto render $x --to pdf
-      # quarto render $x 
-      echo ---------------------
-   done 
--- materials/commands.sh
+++
@@ -1,3 +0,0 @@
-quarto render 3_i_introToPython_solutions.qmd  --to pdf
-quarto render 3_ii_introToPython_solutions.qmd --to pdf
-
